acdde983f37df4e17558b2468fbbd0f07d0c9135
Michael Huynh <mhuynh5757@gmail.com>
2018-04-25 02:00:56 -0700

    clearing files for python and TF

--- output.hpp
+++
@@ -1,8 +0,0 @@
-/* Header file for the ouput method */
-#include <vector>
-#include <string>
-
-using namespace std;
-
-/* Outputs a vector<float> of predictions to a given file */
-void output(std::vector<float> predictions, string file);
